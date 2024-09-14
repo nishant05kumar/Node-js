d71ee8098f60f1d78aa2ba670878a8da5398e84f
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-14 08:37:58 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -2,3 +2,23 @@
 
 //alert( 3 + 3 ) // we are using node , not browser
 
+console.log(3+3)
+console.log("Rachana Nishant")
+
+let name = "nishant"
+let age = 21
+let isLoggedIn = false
+let state; // it means undefiened
+
+console.log(typeof "nishant")       // to now the datatype we use typeof 
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// datatyper
+//numbers = 2 to power 5
+// bigint
+// strig
+// boolean = true / false
+// null = standalone value
+// undefiened 
