bb80f5006c96d44b3e37ef3b13d1cbf1b9affc2f
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-16 17:26:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = null;
+
+//console.log(typeof score) 
+//console.log(typeof (score)) // will change according to the variable value
+
+
+let valueInNumber = Number(score) // it will convert it into number , string and others 
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" = 33
+// "33abc"= NaN
+// true = 1 , false = 0
+// null = object 
+
+
+ let isLoggedIn = 1 
+ let boolIn = Boolean(isLoggedIn)
+// console.log(boolIn)
+
+ let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
+ // so we can convert it into any form 
