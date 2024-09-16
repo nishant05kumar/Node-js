b5d5c56f43e0db7ad271343c0d1e774de2143533
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-16 18:24:57 +0000

    operation datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,7 +20,45 @@ let valueInNumber = Number(score) // it will convert it into number , string and
 
  let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(stringNumber)
- console.log(typeof stringNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)
 
  // so we can convert it into any form 
+
+
+ // ***************************** OPERATIONS ************************************
+
+let value = 3 
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2^3)
+
+let str1 = "rachana "
+let str2 = "nishant"
+
+let str3 = str1+str2
+//console.log(str3)
+
+
+// console.log("1" + 2); // it will print 12
+// console.log(1 + "2"); // it print 12
+// console.log("1" + 2 + 2); // it treat all as string bcz of starting element is string it will print 122
+// console.log(1 + 2 + "2"); // it will first add then concate the string and print 32
+
+// console.log((2+3) * (5/9));
+
+// console.log(true);
+// console.log("");
+
+let num1 , num2, num3 
+ num1 = num2 = num3 = 2+2
+ //console.log(num2);
+ 
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
