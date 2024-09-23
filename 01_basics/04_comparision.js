bff88b9da792d2a4995138e985249ee6ffa1b738
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-23 10:58:40 +0000

    comparision done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+//console.log(2>3)
+
+// console.log("2" > 1)
+// console.log("2" > 5)
+
+//console.log(null > 0); 
+//console.log(null == 0); // for null comparision use == for the result... don;t use single = it will not compile.
+//console.log(null >= 0);
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined < 0)
+
+// // === strictly check krta hai and  data type bhi check krte hai  fir result deta hai
+
+console.log("2" === 2) 
