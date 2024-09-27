c62865ba90becde2e4e869a605cb2f5d18845e2c
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-27 06:04:49 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,19 @@
+//  old CONCATINATION 
+
+const  name = "nishant"
+const Count = 50                // this is the old version of concatination 
+
+//console.log(name + Count)
+
+// NEW CONCATINATION 
+
+console.log(`my name is ${name} and my repo count is ${Count}`)     // new way of concatination 
+
+const newGame = new String("nishanannt")         // making new string
+//console.log(newGame)
+console.log(newGame[4])                        // to know the character place
+console.log(newGame.__proto__)
+console.log(newGame.length)
+console.log
+
+
