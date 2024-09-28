c14bbb87016294fe2f48d07555c0366232ebca91
Nishant Kumar <131908189+nishant05kumar@users.noreply.github.com>
2024-09-28 15:50:52 +0000

    strings done

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -7,13 +7,28 @@ const Count = 50                // this is the old version of concatination
 
 // NEW CONCATINATION 
 
-console.log(`my name is ${name} and my repo count is ${Count}`)     // new way of concatination 
+//console.log(`my name is ${name} and my repo count is ${Count}`)     // new way of concatination 
 
-const newGame = new String("nishanannt")         // making new string
+const newGame = new String("Nishant-nt")         // making new string
 //console.log(newGame)
-console.log(newGame[4])                        // to know the character place
-console.log(newGame.__proto__)
-console.log(newGame.length)
-console.log
+// console.log(newGame[4])                        // to know the character place
+// console.log(newGame.__proto__)
+// console.log(newGame.length)
+// console.log(newGame.toUpperCase())
+// console.log(newGame.charAt(7))
+// console.log(newGame.indexOf('n'))
+const newString = newGame.substring(0,5)   // it will print n-1.  , starting should be zero always
+//console.log(newString)
 
+const anotherString = newGame.slice(-8,4)
+//console.log(anotherString)
+const newStringOne = "  nishant  "
+//console.log(newStringOne)
+//console.log(newStringOne.trim())
+
+const url = "http://nishant.com/nishant%20kumar"
+console.log(url.replace('%20','-puchi-'))               // we can replace in any thing 
+console.log(url.includes('nis'))                  // to know that it is present or not in that
+
+console.log(newGame.split('-'))
 
